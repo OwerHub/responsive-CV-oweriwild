402c3b5589b33b8af162deb98f96f481b6198057
OwerWild <owerwild@gmail.com>
2022-08-29 06:37:21 +0200

    IndabidAD add to references

--- src/service/referenceService.jsx
+++ src/service/referenceService.jsx
@@ -3,8 +3,9 @@ import resCvPicIncognito from "../img/pics/response-cv-1.jpg";
 import hoolieDayz from "../img/pics/hoolieDazy.jpg";
 import parallaxPizza from "../img/pics/parallax-pizza.jpg";
 import showRoomPic from "../img/pics/showroom-demo.jpg";
+import inhabitADPic from "../img/pics/PomsCloudSquare.jpg"
 
-const projectPictures = [resCvPic, hoolieDayz, parallaxPizza, showRoomPic];
+const projectPictures = [resCvPic, hoolieDayz, parallaxPizza, showRoomPic, inhabitADPic];
 
 export const referencePictures = (number) => {
   /* console.log("in funct", typeof number, number); */
